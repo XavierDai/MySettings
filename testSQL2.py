7bb11944be8e961dac853f165131fab30e208a59
XavierDai <113158408+XavierDai@users.noreply.github.com>
2023-06-02 10:39:49 +0800

    Update testSQL2.py

--- testSQL2.py
+++ testSQL2.py
@@ -1,32 +1,41 @@
 import pymysql
-db = pymysql.connect(host="localhost", user="root", password="dai200921", port=3306)
 
-cursor = db.cursor()
+def connect():
+    db = pymysql.connect(host="localhost", user="root", password="dai200921", port=3306)
+    return db
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS test_2")
+def createDatabase(connection):
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS test_2")
 
-sql = "DROP TABLE IF EXISTS test_2.students"
-cursor.execute(sql)
-
-sql = "CREATE TABLE test_2.students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(254) NOT NULL, age INT NULL)"
-cursor.execute(sql)
-
-
-
-sql = "INSERT INTO test_2.students (name,age) VALUES ('dai',22)"
-cursor.execute(sql)
-
-
-
-sql = "SELECT * FROM test_2.students WHERE age >= 20"
-
-try:
+def dropTable(connection):
+    cursor = connection.cursor()
+    sql = "DROP TABLE IF EXISTS test_2.students"
     cursor.execute(sql)
-    print("count:", cursor.rowcount)
-    row = cursor.fetchone()
-    while row:
-        print("Row:", row)
-        row = cursor.fetchone()
-except:
-    print('error')
 
+def createTable(connection):
+    cursor = connection.cursor()
+    sql = "CREATE TABLE test_2.students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(254) NOT NULL, age INT NULL)"
+    cursor.execute(sql)
+
+def insertData(connection):
+    cursor = connection.cursor()
+    sql = "INSERT INTO test_2.students (name,age) VALUES ('dai',22)"
+    cursor.execute(sql)
+
+def searchData(connection):
+    cursor = connection.cursor()
+    sql = "SELECT * FROM test_2.students WHERE age >= 20"
+    try:
+        cursor.execute(sql)
+        print("count:", cursor.rowcount)
+        row = cursor.fetchone()
+        while row:
+            print("Row:", row)
+            row = cursor.fetchone()
+    except:
+        print('error')
+
+def main():
+    connection = connect()
+    createDatabase(connection)
