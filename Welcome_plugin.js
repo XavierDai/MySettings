55e8e23913d8883125f92d5fbd4dbe538cee554f
XavierDai <daiyilong@ufl.edu>
2023-06-15 16:49:09 +0800

    update

--- Welcome_plugin.js
+++ Welcome_plugin.js
@@ -7,8 +7,11 @@ class ControlEntity{
 
 const plugin = ({ widgets, simulator, vehicle }) => {
   var txt = readTextFile("http://127.0.0.1:5500/ident.txt");
+  var allDriverDictionary = analysisTxt(txt);
+  var defaultDriverDictionary = allDriverDictionary["Default_Driver"]
 
-  analysisTxt(txt);
+  console.log(defaultDriverDictionary["seatPosition"]);
+  var systemDictionary = {};
   // live args
   var selectedDriverName = null;
   var isPersonal = false;
@@ -33,6 +36,8 @@ const plugin = ({ widgets, simulator, vehicle }) => {
   var mirrorRightPan = null;
   var ACAirFlowLevel = null;
   var ACTemperature = null;
+  var ACTemperatureIfHot = null;
+  var ACTemperatureIfCold = null;
   var SteeringWheelWarm = null;
   var SeatHeatLevel = null;
   var SeatVentilation = null;
@@ -40,8 +45,12 @@ const plugin = ({ widgets, simulator, vehicle }) => {
   var SteeringMode = null;
   var BrakingMode = null;
   var PowerMode = null;
+  var driveMode = null;
 
-
+  function fixedDriverPicked(pickedDriverName){
+    systemDictionary = allDriverDictionary[pickedDriverName];
+    console.log(systemDictionary);
+  }
 
   var afterTempJudge = {
     val: "Set AC Air Flow",
@@ -868,249 +877,8 @@ const plugin = ({ widgets, simulator, vehicle }) => {
     box.injectNode(personalSettingModule);
   });
 
-  // widget for externalSetting
-  const externalSettingModule = document.createElement("div");
-  externalSettingModule.setAttribute("style", `height: 100%; width: 100%;`);
-  externalSettingModule.innerHTML = `
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        </head>
 
-        <body>
-            <div class="card" style="margin-left:10px;margin-right:10px;margin-top:5px;">
-                <h5 class="card-title">External Setting</h5>
-                <span class="placeholder col-12 bg-info placeholder-xs"></span>
-
-                <label for="rainRange" class="form-label">Rain Intensity (0% ~ 100%)：</label>
-                <span id="rainRangeDisplay">
-                    50
-                </span>%
-                </br>
-                <input type="range" class="form-range w-50" min="0" max="100" id="rainRange">
-
-                <label for="temperatureRange" class="form-label">Outside Temperature (-50°C ~ +50°C)：</label>
-                <span id="temperatureRangeDisplay">
-                    0
-                </span>°C
-                </br>
-                <input type="range" class="form-range w-50" min="-50" max="50" id="temperatureRange">
-            </div>
-        
-            <script src=https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        </body>   
-    `;
-
-  var RainIntensityRange = externalSettingModule.querySelector("#rainRange");
-  RainIntensityRange.addEventListener("click", function (event) {
-    var rainIntensity = event.target.value;
-    console.log("rainIntensity:", rainIntensity);
-    externalSettingModule.querySelector("#rainRangeDisplay").innerHTML =
-      rainIntensity;
-  });
-
-  var temperatureRange =
-    externalSettingModule.querySelector("#temperatureRange");
-  temperatureRange.addEventListener("click", function (event) {
-    var OutsideTemperature = event.target.value;
-    console.log("OutsideTemperature:", OutsideTemperature);
-    externalSettingModule.querySelector("#temperatureRangeDisplay").innerHTML =
-      OutsideTemperature;
-  });
-
-  widgets.register("Setting2", (box) => {
-    box.injectNode(externalSettingModule);
-  });
-
-  // widget for car display1
-  const displayModule1 = document.createElement("div");
-  displayModule1.setAttribute("style", `height: 100%; width: 100%;`);
-  displayModule1.innerHTML = `
-        <div id="switch1" style="height:100px;padding: 20px; text-align:center;background-color:red">
-            <div style="font-size: 18px;">
-                Webpage1
-            </div>
-            <form id="switch-form">
-                <button class="btn btn-outline-primary w-30" type="submit">Switch</button>
-            </form>
-        </div>
-        <div id="switch2" style="height:100px;padding: 20px; text-align:center;background-color:blue;display:none">
-            <div style="font-size: 18px;">
-                HeadLight: <span id="headlight">OFF</span>
-            </div>
-            <div style="margin-top: 10px;font-size: 16px;">
-                <button id="btn_head_light_on" style="padding: 8px; margin-left: 8px;background-color:blue;color:white;">ON</button>
-                <button id="btn_head_light_off" style="padding: 8px; margin-left: 8px;background-color:red;color:white;">OFF</button>
-            </div>
-        </div>
-    `;
-  var switchForm = displayModule1.querySelector("#switch-form");
-  switchForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the form from submitting
-    var switch1 = displayModule1.querySelector("#switch1");
-    var switch2 = displayModule1.querySelector("#switch2");
-    switch1.style.display = "none";
-    switch2.style.display = "block";
-  });
-  widgets.register("Display1", (box) => {
-    box.injectNode(displayModule1);
-  });
-
-  // widget for car display2
-  const displayModule2 = document.createElement("div");
-  displayModule2.setAttribute("style", `height: 100%; width: 100%;`);
-  displayModule2.innerHTML = `
-        <div style="height:100px;padding: 20px; text-align:center;">
-            <div style="font-size: 18px;">
-                HeadLight: <span id="headlight">OFF</span>
-            </div>
-            <div style="margin-top: 10px;font-size: 16px;">
-                <button id="btn_head_light_on" style="padding: 8px; margin-left: 8px;background-color:blue;color:white;">ON</button>
-                <button id="btn_head_light_off" style="padding: 8px; margin-left: 8px;background-color:red;color:white;">OFF</button>
-            </div>
-        </div>
-    `;
-  widgets.register("Display2", (box) => {
-    box.injectNode(displayModule2);
-  });
-
-  // const personModule =document.createElement("div")
-  // personModule.setAttribute("style", `height: 100%; width: 100%;`)
-  // personModule.innerHTML=(`
-  //     <head>
-  //         <meta charset="utf-8">
-  //         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  //         <style>
-  //             .driver-name{
-  //                 font-size:4.5vw;
-  //             }
-  //             .driver-description{
-  //                 font-size:4.5vw;
-  //             }
-  //             .driver-card-h5{
-  //                 font-size:6vw;
-  //             }
-  //             .list-group-item:hover{
-  //                 cursor:pointer;
-  //             }
-  //         </style>
-
-  //         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  //     </head>
-
-  //     <body>
-  //         <div class="card" style="margin-left:10px;margin-right:10px;margin:top:10px;">
-  //             <h5 class="card-header driver-card-h5">Pick a driver</h5>
-  //             <div class="card-body">
-  //                 <div id="multipleUsersDiv"></div>
-  //             </div>
-  //         </div>
-  //         <script src=https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  //         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-  //         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  //     </body>
-  // `)
-
-  // var personDic = {"Person1":["Yilong Dai","xxxxxxxxx"],"Person2":["Kyrie Irving","Asdadasdadadasd"],"Person3":["Ziyi Wang","abaaba"]};
-
-  // var html = '';
-  // html+=`<div class="row">
-  //             <div class="col-5">
-  //                 <div class="list-group driver-name" id="list-tab" role="tablist">`
-  // for(var key in personDic){
-  //     html+='<a class="list-group-item list-group-item-action " id="'+key+'" data-bs-toggle="list" role="tab" aria-controls="'+personDic[key][1]+'Content">'+personDic[key][0]+'</a>'
-  // }
-  // html+=`
-  //     </div>
-  //             </div>
-  //         <div class="col-7">
-  //             <div class="tab-content driver-description" id="nav-tabContent">
-  // `
-  // for(var key in personDic){
-  //     html+='<div class="tab-pane fade " id="'+key+'Content" role="tabpanel" aria-labelledby="'+key+'">'+personDic[key][1]+'</div>'
-  // }
-  // html+=`
-  // </div>
-  //         </div>
-  //       </div>
-  // `
-  // personModule.querySelector('#multipleUsersDiv').innerHTML = html;
-
-  // var previousTab = null;
-  // var previousContent = null;
-  // for(var key in personDic){
-
-  //     personModule.querySelector('#'+key).addEventListener('click',function(event){
-  //         console.log(this.id)
-
-  //         if(previousTab != null){
-  //             previousTab.setAttribute("class","list-group-item list-group-item-action");
-  //             previousContent.setAttribute("class","tab-pane fade");
-  //         }
-  //         previousTab = personModule.querySelector('#'+this.id);
-  //         previousContent = personModule.querySelector('#'+this.id+"Content");
-  //         previousTab.setAttribute("class","list-group-item list-group-item-action active");
-  //         previousContent.setAttribute("class","tab-pane fade show active");
-  //     })
-  //     console.log(key)
-
-  // }
-
-  // widgets.register("Person",
-  //     (box) =>{
-  //     box.injectNode(personModule)
-  // })
-
-  const subscribeHighBeamLight = document.createElement("div");
-  subscribeHighBeamLight.setAttribute(
-    "style",
-    `height: 100%; width: 100%;color:red`
-  );
-  subscribeHighBeamLight.innerHTML = `
-        <div style="height:100px;padding: 20px; text-align:center;">
-        <div style="font-size: 18px;">HeadLight: <span id="headlight">OFF</span></div>
-        <div style="margin-top: 10px;font-size: 16px;">
-        <button id="btn_head_light_on" style="padding: 8px; margin-left: 8px;background-color:blue;color:white;">ON</button>
-        <button id="btn_head_light_off" style="padding: 8px; margin-left: 8px;background-color:red;color:white;">OFF</button>
-        </div>
-        </div>
-    `;
-  widgets.register("Headlight", (box) => {
-    box.injectNode(subscribeHighBeamLight);
-  });
-
-  const lightState = subscribeHighBeamLight.querySelector("#headlight");
-  let btnLightOn = subscribeHighBeamLight.querySelector("#btn_head_light_on");
-  let btnLightOff = subscribeHighBeamLight.querySelector("#btn_head_light_off");
-  // const listeners = [];
-
-  if (btnLightOn) {
-    btnLightOn.addEventListener("click", () => {
-      for (const listener of listeners) {
-        //'Body.Lights.IsHighBeamOn'
-        listener("True");
-      }
-    });
-  }
-
-  if (btnLightOff) {
-    btnLightOff.addEventListener("click", () => {
-      for (const listener of listeners) {
-        //'Body.Lights.IsHighBeamOn'
-        listener("False");
-      }
-    });
-  }
-
-  setInterval(async () => {
-    let value = await vehicle["Body.Lights.IsHighBeamOn"].get();
-    lightState.innerHTML = value ? "ON" : "OFF";
-  }, 1000);
-
+  
   const flowChartModule = document.createElement("div");
   flowChartModule.innerHTML = `
     <head>
@@ -1677,11 +1445,11 @@ const plugin = ({ widgets, simulator, vehicle }) => {
                                             Seat Position:&nbsp;
                                         </span>
                                         <span id="seatPositionRangeDisplay" style="text-decoration:underline;">
-                                            50
+                                            ` + defaultDriverDictionary["seatPosition"] + `
                                         </span>
                                     </div>
                                     <div class="col-5">
-                                        <input type="range" class="form-range float-end" min="-50" max="50" id="SeatPositionRange">
+                                        <input type="range" class="form-range float-end" min="-50" max="50" id="SeatPositionRange" value="`+ defaultDriverDictionary["seatPosition"] +`">
                                     </div>
                                     
                                 </div>
@@ -1694,14 +1462,32 @@ const plugin = ({ widgets, simulator, vehicle }) => {
                                     <div class="row g-3 align-items-center">
                                         <div class="col-7">
                                             <span class="float-start" style="margin-left:8px">
-                                                AC temperature:&nbsp;
+                                                AC temperature if cold:&nbsp;
                                             </span>
-                                            <span id="ACTemperatureRangeDisplay" style="text-decoration:underline;">
+                                            <span id="ACTemperatureIfColdRangeDisplay" style="text-decoration:underline;">
                                                 50
                                             </span>°C
                                         </div>
                                         <div class="col-5">
-                                            <input type="range" class="form-range float-end" min="-50" max="50" id="ACTemperatureRange">
+                                            <input type="range" class="form-range float-end" min="-50" max="50" id="ACTemperatureIfColdRange">
+                                        </div>
+                                    </div>
+                            </td>
+                        </tr>
+                        <tr>
+                  
+                            <td>
+                                    <div class="row g-3 align-items-center">
+                                        <div class="col-7">
+                                            <span class="float-start" style="margin-left:8px">
+                                                AC temperature if hot:&nbsp;
+                                            </span>
+                                            <span id="ACTemperatureIfHotRangeDisplay" style="text-decoration:underline;">
+                                                50
+                                            </span>°C
+                                        </div>
+                                        <div class="col-5">
+                                            <input type="range" class="form-range float-end" min="-50" max="50" id="ACTemperatureIfHotRange">
                                         </div>
                                     </div>
                             </td>
@@ -1953,12 +1739,20 @@ const plugin = ({ widgets, simulator, vehicle }) => {
     seatPosition.value;
   });
 
-  var ACTemperatureRange = bigBoxModule.querySelector("#ACTemperatureRange");
-  ACTemperatureRange.addEventListener("click", function (event) {
-    ACTemperature = event.target.value;
-    console.log("AC Temperature:", ACTemperature);
-    bigBoxModule.querySelector("#ACTemperatureRangeDisplay").innerHTML =
-        ACTemperature;    
+  var ACTemperatureIfHotRange = bigBoxModule.querySelector("#ACTemperatureIfHotRange");
+  ACTemperatureIfHotRange.addEventListener("click", function (event) {
+    ACTemperatureIfHot = event.target.value;
+    console.log("AC Temperature IfHot:", ACTemperatureIfHot);
+    bigBoxModule.querySelector("#ACTemperatureIfHotRangeDisplay").innerHTML =
+        ACTemperatureIfHot;    
+  });
+
+  var ACTemperatureIfColdRange = bigBoxModule.querySelector("#ACTemperatureIfColdRange");
+  ACTemperatureIfColdRange.addEventListener("click", function (event) {
+    ACTemperatureIfCold = event.target.value;
+    console.log("AC Temperature IfCold:", ACTemperatureIfCold);
+    bigBoxModule.querySelector("#ACTemperatureIfColdRangeDisplay").innerHTML =
+        ACTemperatureIfCold;    
   });
 
   var ACAirFlowLevelRange = bigBoxModule.querySelector("#ACAirFlowLevelRange");
@@ -2064,8 +1858,8 @@ const plugin = ({ widgets, simulator, vehicle }) => {
 
 
   var personDic = {
-    Person1: ["Yilong Dai", "Me"],
-    Person2: ["Kyrie Irving", "NBA all star"],
+    Person1: ["Yilong_Dai", "Me"],
+    Person2: ["Kyrie_Irving", "NBA all star"],
     Person3: ["Driver by you", "Customize your own"],
   };
 
@@ -2121,6 +1915,8 @@ const plugin = ({ widgets, simulator, vehicle }) => {
         console.log(this.id);
         bigBoxModule.querySelector("#pickDriver").style.border="1px solid #CCCCCC";
         bigBoxModule.querySelector("#pickDriver").style.boxShadow = "0px 0px 0px 0px";
+        alertDiv.setAttribute("class","toast");
+        fixedDriverPicked(personDic[this.id][0]);
         if (previousTab != null) {
           previousTab.setAttribute(
             "class",
@@ -2178,9 +1974,7 @@ const plugin = ({ widgets, simulator, vehicle }) => {
     }
   }
 
-  function fixedDriverPicked(pickedDriverName){
-
-  }
+  
 
 
   simulator(
@@ -2410,11 +2204,9 @@ function analysisTxt(txt){
   let identName = null;
   let key;
   let value;
-  console.log(txt);
+
   for (var i=0; i < txt.length; i++){
-    console.log(txt[i]);
     identInfo = txt[i].split("#");
-    console.log(identInfo);
     identName = identInfo.shift().split(":")[1]
     for(var j=0; j<identInfo.length;j++){
       key = identInfo[j].split(":")[0];
@@ -2422,6 +2214,7 @@ function analysisTxt(txt){
       dictionaryInfo[key] = value;
     }
     dictionaryDriver[identName] = dictionaryInfo;
+    dictionaryInfo = {};
   }
 
   console.log(dictionaryDriver);
